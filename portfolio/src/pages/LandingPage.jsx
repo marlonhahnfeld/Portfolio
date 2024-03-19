d1f771e8012351d4b8a6ea22530f7bd876ae15b3
Marlon Hahnfeld <130953452+marlonhahnfeld@users.noreply.github.com>
2024-03-19 20:32:11 +0100

    vite react init
    tailwind init
    
    folder structure

---
+++ portfolio/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen bg-slate-900">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-5xl font-bold text-white">Hello, World!</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
