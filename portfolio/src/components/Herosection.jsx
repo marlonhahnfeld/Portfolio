ce48e420174ab85de0d392387203509d37407131
marlon <marlon.misiak-hahnfeld@haw-hamburg.de>
2024-03-27 23:54:49 +0100

    added react three drei/fiber and framer
    replaced tech stack with skills

--- portfolio/src/components/Herosection.jsx
+++ portfolio/src/components/Herosection.jsx
@@ -1,27 +1,33 @@
-import Skills from "./Skills";
+import SkillsTimeline from "./SkillsTimeline";
 
 const HeroSection = () => {
   return (
-    <main className="h-auto flex-grow bg-slate-950 flex flex-col justify-center items-center text-white p-4">
+    <main className="h-auto flex-grow bg-slate-950 flex flex-col justify-center items-center text-white p-4 relative">
       <h1 className="text-4xl font-bold mb-4 font-figtree mt-80">
         Hallo, ich bin
       </h1>
-      <h1 className="text-4xl font-bold font-figtree  bg-gradient-to-r from-purple-500 via-blue-500 to-pink-500 bg-clip-text text-transparent bg-glow mb-96 hover:text-white">
+      <h1 className="text-4xl font-bold font-figtree  bg-gradient-to-r from-purple-500 via-blue-500 to-pink-500 bg-clip-text text-transparent bg-glow mb-96 ">
         Marlon
       </h1>
 
-      <div className="relative flex justify-center items-center text-green-400 font-bold text-2xl my-12 w-64">
-        <img
-          src="src/assets/hand-drawn-circle-red.png"
-          alt="circle"
-          className="absolute"
-        />
-        <p className="z-10 text-4xl font-figtree">Techstack</p>
+      <div className="relative flex flex-row justify-center items-center font-bold text-2xl my-12 w-full">
+        <div className="w-1/2 flex flex-col items-center justify-center text-white">
+        <div className="circleplustext flex justify-center items-center relative">
+            <img
+              src="src/assets/hand-drawn-circle-red.png"
+              alt="circle"
+              className="w-96 h-32"
+            />
+            <p className="z-10 text-4xl font-figtree absolute inset-0 flex justify-center items-center">
+              Skills
+            </p>
+          </div>
+          <SkillsTimeline />
+        </div>
       </div>
-      <Skills />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-64">
-        Click me
-      </button>
+      
+      <p className="text-white text-4xl">Projects</p>
+
     </main>
   );
 };
---
+++ portfolio/src/components/SkillsTimeline.jsx
@@ -0,0 +1,15 @@
+import Timeline from "./Timeline";
+
+
+const SkillsTimeline =() => {
+return(
+    <>
+    <div className="flex flex-row justify-center w-2/3 h-96 mt-16 mb-96 text-green-400  ">
+      <Timeline />
+  </div>
+ 
+  </>
+);
+
+}
+export default SkillsTimeline;
